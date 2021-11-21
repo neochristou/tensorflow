92241f2b7907538fa9e6afa0154df9d21a280469
Neophytos Christou <neophytos_christou@brown.edu>
2021-11-21 17:42:47 -0500

    Refactor run tests script to use pool multiprocessing

--- fuzz-utils/run_kernel_tests.py
+++ fuzz-utils/run_kernel_tests.py
@@ -4,131 +4,115 @@ import signal
 import subprocess
 import time
 from glob import glob
-from multiprocessing import Lock, Manager, Process, current_process
+from multiprocessing import Lock, Manager, Pool, Process, current_process
 
 NUM_PARALLEL_PROCESSES = 5
-TIME_LIMIT = 10800
+NUM_CRASHES = 3
+TIME_LIMIT = 900
 RES_FOLDER = "/media/tf-fuzzing/results/"
 DONE_FOLDER = "/media/tf-fuzzing/done-tests/"
-TEST_FOLDER = "/media/mlfuzz/tensorflow/tensorflow/python/"
-ABRT_FILE = "/media/tf-fuzzing/aborted.txt"
+PYTHON_TEST_FOLDER = "/media/mlfuzz/tensorflow/tensorflow/python/"
+CC_TEST_FOLDER = "/media/mlfuzz/tensorflow/bazel-out/k8-opt/bin/tensorflow/core/kernels/"
+# ABRT_FILE = "/media/tf-fuzzing/aborted.txt"
+TEST_DURATION_FILE = "/media/tf-fuzzing/test_durations.txt"
 
-tests = glob(TEST_FOLDER + "**/*_test*.py", recursive=True)
-TOTAL_TESTS = len(tests)
-running_tests = []
-done_tests = 0
+tests_to_run = glob(PYTHON_TEST_FOLDER + "**/*_test*.py", recursive=True)
+tests_to_run += glob(CC_TEST_FOLDER + "*_test")
+
+TOTAL_TESTS = len(tests_to_run)
+
+done_tests = []
 crashes_set = set()
 aborts_set = set()
 killed_set = set()
 
-
-def execute(test, proc_ecodes, pidx):
-    global lock
-    completed = subprocess.run(["python3", test],
-                               # stdout=subprocess.DEVNULL,
-                               stderr=subprocess.DEVNULL
-                               )
-    if completed.returncode == -signal.SIGABRT:
-        logging.critical(f"Test {test} aborted")
-        lock.acquire()
-        try:
-            f = open(ABRT_FILE, 'a')
-            f.write(test + "\n")
-            f.close()
-        finally:
-            lock.release()
-
-    proc_ecodes[pidx] = completed.returncode
+crash_counts = {}
 
 
-def main():
+def execute(test, proc_ecodes, pidx, start_time):
+
+    args = [test]
+
+    if PYTHON_TEST_FOLDER in os.path.realpath(test):
+        args.insert(0, "python3")
+
+    retcode = 0
+
+    try:
+        completed = subprocess.run(args,
+                                   stdout=subprocess.DEVNULL,
+                                   stderr=subprocess.DEVNULL,
+                                   timeout=TIME_LIMIT)
+        retcode = completed.returncode
+    except subprocess.TimeoutExpired as e:
+        logging.info(f"Timeout expired for {test}")
+        retcode = -9
+    finally:
+        return (test, time.time() - start_time, retcode)
+
+
+def log_test_duration(dur, test):
+    f = open(TEST_DURATION_FILE, 'a')
+    mins = int(dur) / 60
+    f.write(test + " " + str(mins) + "\n")
+    f.close()
+
+
+def proc_finished(results):
+
+    test, running_time, exitcode = results
+
+    done_tests.append(test)
+    log_test_duration(running_time, test)
+
+    if exitcode == -signal.SIGABRT:
+        aborts_set.add(test)
+
+    # If the test crashed, re-queue it so that it runs again
+    if exitcode < 0:
+
+        if test not in crash_counts:
+            crash_counts[test] = 0
+
+        crash_counts[test] += 1
+        crashes_set.add(test)
+        logging.info(
+            f"Test {test} crashed with exit code {exitcode}, requeueing")
+
+        if crash_counts[test] < NUM_CRASHES:
+            tests_to_run.append(test)
+
+
+if __name__ == "__main__":
+
     logging.basicConfig(level=logging.DEBUG)
-    lock = Lock()
+    # lock = Lock()
     manager = Manager()
+    pool = Pool(processes=NUM_PARALLEL_PROCESSES)
     # Used to save the test exit codes
     proc_ecodes = manager.dict()
     proc_ids = {}
     proc_id = 0
 
-    while done_tests < TOTAL_TESTS:
+    # Save an id and the start time of the process
+    # proc_ids[p] = (proc_id, time.time())
+    # proc_id += 1
 
-        # Use this to log status if a test is done
-        changed = False
+    while len(done_tests) < TOTAL_TESTS:
 
-        # logging.debug("Monitoring running processes")
-        procs_to_remove = []
-        tests_to_remove = []
-        for idx, proc in enumerate(proc_ids.keys()):
+        while len(tests_to_run) > 0:
 
-            # A test has finished running, handle it
-            if not proc.is_alive():
-                procs_to_remove.append(proc)
-                t = running_tests[idx]
-                # Remove it after the loop so as not to mess with the indices
-                tests_to_remove.append(t)
-                # Save the exitcode in the dictionary
-                exitcode = proc_ecodes[proc_ids[proc][0]]
+            test_to_run = tests_to_run.pop()
+            p = pool.apply_async(execute, args=(
+                test_to_run, proc_ecodes, proc_id, time.time()),
+                callback=proc_finished,
+                error_callback=proc_finished)
 
-                if exitcode == -signal.SIGABRT:
-                    aborts_set.add(t)
-
-                if exitcode == -signal.SIGKILL:
-                    killed_set.add(t)
-
-                # If the test crashed, re-queue it so that it runs again
-                if exitcode < 0:
-                    crashes_set.add(t)
-                    logging.info(
-                        f"Test {t} crashed with exit code {exitcode}")
-                    tests.insert(0, t)
-
-                # If it didn't crash, it has finished
-                else:
-                    done_tests += 1
-                    changed = True
-
-            # Check if the test has been running for too long and kill them
-            start_time = proc_ids[proc][1]
-            if time.time() - start_time > TIME_LIMIT:
-                logging.debug(
-                    f"{running_tests[idx]} exceeded time limit, killing")
-                killed_set.add(proc)
-                proc.kill()
-
-        # Remove the finished processes and tests
-        proc_ids = {p: e for p, e in proc_ids.items()
-                    if p not in procs_to_remove}
-        running_tests = [t for t in running_tests if t not in tests_to_remove]
-
-        while len(running_tests) < NUM_PARALLEL_PROCESSES and done_tests < TOTAL_TESTS:
-            test_to_run = tests.pop()
-            logging.info(f"Preparing to run test {test_to_run}")
-            running_tests.append(test_to_run)
-            p = Process(target=execute, args=(
-                test_to_run, proc_ecodes, proc_id))
-
-            # Save an id and the start time of the process
-            proc_ids[p] = (proc_id, time.time())
-            proc_id += 1
-            p.start()
-
-        if changed:
-            logging.info(f"Finished tests so far: {done_tests}")
-            logging.info(f"Total crashes: {len(crashes_set)}")
+            # logging.info(f"Total crashes: {len(crashes_set)}")
             # logging.info(f"Of which are aborts: {len(aborts_set)}")
             # logging.info(f"Of which were killed: {len(killed_set)}")
 
         time.sleep(10)
-
-    for p in proc_ids.keys():
-        p.join()
-
-    # Also run native tests
-    subprocess.run(["bazel", "test", "//tensorflow/core/kernels:all",
-                    "--test_output=all", "--cache_test_results=no", "--runs_per_test=5"])
+        logging.info(f"Finished tests so far: {len(done_tests)}")
 
     logging.info(f"Total tests run: {done_tests}")
-
-
-if __name__ == "__main__":
-    main()
